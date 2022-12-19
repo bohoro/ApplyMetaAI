2513b9a2113090f36927ddf8f34dab33892a374e
Brian O'Horo <brian.ohoro@gmail.com>
2022-12-19 12:30:01 -0800

    Minor Format Updates

--- GALACTICA/galactica_demo.py
+++ GALACTICA/galactica_demo.py
@@ -3,12 +3,10 @@ import os
 import streamlit as st
 from transformers import AutoTokenizer, OPTForCausalLM
 
-# ############################################################################
-# Initialize the model
-# ############################################################################
-# facebook/galactica-1.3b, facebook/galactica-6.7b, facebook/galactica-30b
-fb_model = "facebook/galactica-6.7b"
+# Define the model (size)
+fb_model = "facebook/galactica-6.7b"  # also 1.3b, 30b, and 120b
 
+# if the model is not already in the session state, create and store it
 if "tokenizer" not in st.session_state:
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     with st.spinner("Loading Galactica Tokenizer and Model..."):
@@ -21,6 +19,7 @@ if "tokenizer" not in st.session_state:
 tokenizer = st.session_state["tokenizer"]
 model = st.session_state["model"]
 
+# dictionary to map user capabiltiy input to required prompt text
 capabilities_map = {
     "Predict Citations": "[START_REF]",
     "Predict LaTeX": " \\[",
@@ -30,29 +29,26 @@ capabilities_map = {
 }
 
 
-# create_citation, runs inference on the input test, returns mdoel output:
-def create_citation(source, capability):
+def run_model(source, capability):
+    """Runs the galactica model
+    Args:
+        source text and capability from drop down
+    Returns:
+        model prediction text
+    """
     tag = capabilities_map[capability]
-
     input_text = f"{source}{tag}"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
-
     outputs = model.generate(input_ids, max_new_tokens=60)
     return tokenizer.decode(outputs[0])
 
 
-# ############################################################################
 # Basic streamlit App
-# ############################################################################
-
 st.title("Galactica Demo", anchor=None)
-
 st.write(
     "GALACTICA is a general-purpose scientific language model trained on a large corpus of scientific text and data."
 )
-
 source = st.text_input("Source", max_chars=512)
-
 st.write(
     "GALACTICA is is not instruction tuned. Because of this you need to use the correct prompts to get good results."
 )
@@ -66,21 +62,19 @@ option = st.selectbox(
         "Question Answering",
     ),
 )
-# Create a text box for the input with a maximum of 512 characters, named "Source":
-
-# Create a button called "Cite Source"
 button = st.button("Submit")
-
-# In the main part of the script, add a conditional statement to check if the button has been clicked
 if button:
-    # Call the create_citation method and store the result in a variable
+    # Call the run_model method and store the result in a variable
     with st.spinner("Galactica inference running..."):
-        citation = create_citation(source.strip(), option)
+        model_output = run_model(source.strip(), option)
 
-    # Set the value of the citations text box to the value of the citation variable
+    # Set the value of the model_outputs text box to the value of the model_output variable
     if option in ("Predict LaTeX", "Reasoning"):
-        st.latex(citation)
+        st.latex(model_output)
     else:
-        citations_box = st.text_area("Citations", citation, height=480, max_chars=8192)
+        model_outputs_box = st.text_area(
+            "model_outputs", model_output, height=480, max_chars=8192
+        )
 
-# Run the Streamlit app using streamlit run galactica_citation_demo.py --server.port 9999
+# Run the Streamlit app using
+# streamlit run galactica_demo.py --server.port 9999
