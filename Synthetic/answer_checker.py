710d21a6aca989c9b8db2d8f0d3d5b4a18afa1a4
Brian O'Horo <brian.ohoro@gmail.com>
2022-12-20 23:05:47 -0800

    Clean Up Synthetic Demo

--- Synthetic/answer_checker.py
+++ Synthetic/answer_checker.py
@@ -4,28 +4,32 @@ import numpy as nn
 import openai
 import streamlit as st
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from sentence_transformers import SentenceTransformer, util
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
+# models uses in demo
 st_model_spec = "all-distilroberta-v1"
+dectector_model = "roberta-base-openai-detector"
+opanai_engine = "text-davinci-002"
 
-# if the model is not already in the session state, create and store it
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# if the models are not already in the session state, initialize and store
 if "tokenizer" not in st.session_state:
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     with st.spinner("Loading Tokenizer and Model..."):
-        st.session_state["tokenizer"] = AutoTokenizer.from_pretrained(
-            "roberta-base-openai-detector"
-        )
+        st.session_state["tokenizer"] = AutoTokenizer.from_pretrained(dectector_model)
         st.session_state["model"] = AutoModelForSequenceClassification.from_pretrained(
-            "roberta-base-openai-detector", device_map="auto"
+            dectector_model, device_map="auto"
         )
         st.session_state["st_model"] = SentenceTransformer(st_model_spec)
-    st.success(f"The Tokenizer and Model Downloaded and Ready!")
+    st.success("The Tokenizer and Model Downloaded and Ready!")
 
 tokenizer = st.session_state["tokenizer"]
 model = st.session_state["model"]
 st_model = st.session_state["st_model"]
 
+# example text for the demo
 example_prompt = """Why was Louis XIV considered an absolute monarch?"""
 example_student_response = """Thus, in religious matters (except where 
 Jansenism was concerned), in his dealings with the nobility and the 
@@ -42,17 +46,21 @@ quasi-spiritual role, symbolized by his consecration with holy oil at
 his coronation, which obliged him to govern justly in accordance with 
 the laws of God and Christian morality."""
 
+# Place your api key in the env var OAI_TOKEN
 openai.api_key = os.environ["OAI_TOKEN"]
 
 
 def get_gpt_response(prompt, length=1024):
     """Get GPT Completion using the prompt"""
-    return """Louis XIV was considered an absolute monarch because he had complete control over the government and the people. He made all the decisions and no one could question his authority."""
-    chatbot_response = (
+    # A helpful rule of thumb is that one token generally corresponds to ~4
+    # characters of text for common English text. For max_tokens, divide chars
+    # by 3 to be conservative.
+    length += int(len(prompt) / 3)
+    api_response = (
         openai.Completion.create(
-            engine="text-davinci-002",
+            engine=opanai_engine,
             prompt=prompt,
-            temperature=0.5,
+            temperature=0.3,
             max_tokens=length,
             top_p=1,
             frequency_penalty=0,
@@ -61,12 +69,12 @@ def get_gpt_response(prompt, length=1024):
         .choices[0]
         .text
     )
-    return chatbot_response
+    return api_response
 
 
 def check_fake(input_text):
     """Use the GPT dector to see if Fake"""
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
 
     with torch.no_grad():
         logits = model(input_ids).logits
@@ -87,6 +95,8 @@ def check_similarity(sent1, sent2):
     return util.cos_sim(emb1, emb2).data[0][0]
 
 
+"""Streamlit UI Code below"""
+
 st.title("Synthetic Output Detector Demo")
 
 # Get the prompt and student response from the user
@@ -95,35 +105,37 @@ student_response = st.text_area(
     "Enter the student's response:",
     value=example_student_response.replace("\r", "").replace("\n", ""),
     max_chars=1024,
+    height=260,
 )
 
-# When the submit button is clicked, run the model and show the prediction
+# When the submit button is clicked, run the models and show the predictions
 if st.button("Submit"):
     if len(student_response) < 500:
         st.warning(
-            "For best results, ensure the student's response has atleast 500 charateres",
+            "For best results, ensure the student's response has at least 500 characters.",
             icon="⚠️",
         )
-    input_text = f"{prompt}\n{student_response}"
-    with torch.no_grad():
-        prediction = "model(input_text)"
-    ai_genenerated = get_gpt_response(prompt, len(student_response)).lstrip()
 
-    st.text_area("GPT-3 Output", value=ai_genenerated)
+    with st.spinner("Running Models..."):
+        # run gpt-3
+        ai_genenerated = get_gpt_response(prompt, len(student_response)).lstrip()
+        st.text_area("GPT-3 Output", value=ai_genenerated)
 
-    # Compare the student's response to the ChatGPT API and display the similarity score
-    similarity = check_similarity(student_response, ai_genenerated)
+        # Compare the student's response to the GPT API, cosine similarity
+        similarity = check_similarity(student_response, ai_genenerated)
 
-    # check the gpt-2 dectector
-    label, score = check_fake(student_response)
-    color = "green"
-    if label == "Fake":
-        color = "red"
+        # check the gpt-2 dectector
+        label, score = check_fake(student_response)
+
+    # write out the Synthetic Output Detector results
+    color = "red" if label == "Fake" else "green"
     st.write(f"### Synthetic Output Detector: **:{color}[{label} {score:.2f}]**")
-    color = "green"
-    if similarity >= 0.9:
-        color = "red"
-    st.markdown(f"### Similarity to ChatGPT: **:{color}[{similarity:.3f}]**")
-    st.markdown(f"Values greater than .9 are very similar.")
+
+    # write out the Similarity results
+    color = "red" if similarity >= 0.9 else "green"
+    st.markdown(f"### Similarity to GPT-3: **:{color}[{similarity:.3f}]**")
+    st.markdown("*Similarity values greater than .9 are very similar.*")
+
+    # If both signals are good, ballons!
     if label == "Real" and similarity < 0.9:
         st.balloons()
