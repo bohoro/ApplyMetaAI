f3b131c0d6df05aef0f6ca1bcfebea6d6e964ebe
Brian O'Horo <brian.ohoro@gmail.com>
2022-12-21 19:47:21 -0800

    Added test script

---
+++ test/run_tests.sh
@@ -0,0 +1,7 @@
+python test_pytorch.py
+python GALACTICA/test_galactica.py
+python Synthetic/test_synthetic.py
+python Synthetic/openai_api_poc.py
+python Synthetic/openai_detector_poc.py
+python Synthetic/test_st_inference.py
+
